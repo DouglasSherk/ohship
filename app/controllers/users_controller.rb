2b0f9af2517e03940db3a93bb67fb568836e2a8c
DouglasSherk <github@sherk.me>
2013-10-29 01:42:53 -0400

    Change name of notice on profile to avoid Devise notifications.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,12 @@ class UsersController < ApplicationController
     @user = current_user
     if !params[:user][:password].blank?
       if @user.update_with_password(user_params)
-        flash[:notice] = 'Successfully changed password.'
+        flash[:change_notice] = 'Successfully changed password.'
         # Usually changing password requires a re-log. Bypass that
         sign_in @user, :bypass => true
       end
     elsif @user.update_attributes(user_params.except :current_password, :password, :password_confirmation)
-      flash[:notice] = 'Successfully changed profile.'
+      flash[:change_notice] = 'Successfully changed profile.'
     end
 
     render action: 'profile'
